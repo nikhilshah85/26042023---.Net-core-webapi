2003f957f70a32c9684ab7df7901a150f03a3caf
Nikhil <nikhilshah.net@hotmail.com>
2023-04-28 18:25:43 +0530

    Day 3 - In memory collection - Objects

---
+++ Day 3/shoppingAPI/shoppingAPI/Controllers/ProductsController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using shoppingAPI.Models;
+namespace shoppingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        Products pObj = new Products(); //this is a very bad code, we shold use DI instead
+
+        [HttpGet]
+        [Route("productlist")]
+        public IActionResult GetAllProducts()
+        {
+            return Ok(pObj.GetAllProducts());
+        }
+
+        [HttpGet]
+        [Route("productlist/searchbyid/{id}")]
+        public IActionResult GetProduct(int id) 
+        {
+            try
+            {
+                return Ok(pObj.GetproductById(id));
+            }
+            catch (Exception es)
+            {
+                return NotFound(es.Message);
+            }            
+        }
+        [HttpGet]
+        [Route("productlist/searchbycategory/{category}")]
+        public IActionResult GetProductByCategory(string category)
+        {
+            return Ok(pObj.GetProductsByCategory(category));
+        }
+
+        [HttpPost]
+        [Route("productlist/add")]
+        public IActionResult AddnewProduct([FromBody]Products newProduct)
+        {
+            try
+            {
+                string result = pObj.AddNewProduct(newProduct);
+                return Created("",result);
+            }
+            catch (Exception es)
+            {
+
+                return Ok(es.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("productlist/edit")]
+        public IActionResult EditeProduct([FromBody] Products productToEdit)
+        {
+            try
+            {
+                string result = pObj.EditProduct(productToEdit);
+                return Accepted(result);
+            }
+            catch (Exception es)
+            {
+                return Ok(es.Message);
+                
+            }
+        }
+
+        [HttpDelete]
+        [Route("productlist/delete/{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            try
+            {
+                string result = pObj.DeleteProduct(id);
+                return Accepted(result);
+            }
+            catch (Exception es)
+            {
+                return Ok(es.Message);                
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
---
+++ Day 3/shoppingAPI/shoppingAPI/Models/Products.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.AspNetCore.Server.HttpSys;
+
+namespace shoppingAPI.Models
+{
+    public class Products
+    {
+        #region Properties
+        public int pId { get; set; }
+        public string pName { get; set; }
+        public string pCategory { get; set; }
+        public int pPrice { get; set; }
+        public bool pIsInStock { get; set; }
+        #endregion
+
+        #region Data Store
+
+        private static List<Products> pList = new List<Products>()
+        {
+            new Products(){ pId=101, pName="Pepsi", pCategory="Cold-Drink", pIsInStock=true, pPrice=50 },
+            new Products(){ pId=102, pName="Appy", pCategory="Cold-Drink", pIsInStock=false, pPrice=90 },
+            new Products(){ pId=103, pName="IPhone", pCategory="Electronics", pIsInStock=true, pPrice=100 },
+            new Products(){ pId=104, pName="FossilQ", pCategory="Watch", pIsInStock=true, pPrice=200 },
+            new Products(){ pId=105, pName="Dell Lattitude", pCategory="Electronics", pIsInStock=true, pPrice=500 },
+            new Products(){ pId=106, pName="Apple Watch", pCategory="Watch", pIsInStock=false, pPrice=1500 },
+        };
+        #endregion
+
+        #region Get Methods
+        public List<Products> GetAllProducts()
+        {
+            return pList;
+        }
+
+        public Products GetproductById(int id)
+        {
+            var checkproduct = pList.Count(p => p.pId == id);
+
+            if (checkproduct == 1)
+            {
+                var prod = (from p in pList
+                            where p.pId == id
+                            select p).Single();
+                return prod;
+            }
+            throw new Exception("Sorry product not found");
+        }
+        public List<Products> GetProductsByCategory(string category)
+        {
+            var prod = (from p in pList
+                        where p.pCategory == category
+                        select p).ToList();
+
+            return prod;
+        }
+
+        #endregion
+
+        public string AddNewProduct(Products newP)
+        {
+            if (newP.pPrice < 0)
+            {
+                throw new Exception("Please enter a valid price for your product");
+            }
+            else if (newP.pName.Length < 3)
+            {
+                throw new Exception("Please enter product name more than 3 characters");
+            }
+            //do other validations
+            pList.Add(newP);
+            return "Product Added Successfully";
+        }
+
+        public string EditProduct(Products productToEdit)
+        {
+            var count = pList.Count(p => p.pId == productToEdit.pId);
+
+            if (count > 0)
+            {
+                var p = (from pr in pList
+                         where pr.pId == productToEdit.pId
+                         select pr).Single();
+
+                p.pName = productToEdit.pName;
+                p.pCategory = productToEdit.pCategory;
+                p.pPrice = productToEdit.pPrice;
+                p.pIsInStock = productToEdit.pIsInStock;
+                return "Product Edited Succsfully";
+            }
+            throw new Exception("Product Not Found");
+        }
+
+        public string DeleteProduct(int id)
+        {
+            var count = pList.Count(p => p.pId == id);
+            if (count > 0)
+            {
+                var p = (from pr in pList
+                         where pr.pId == id
+                         select pr).Single();
+
+                pList.Remove(p);
+                return "Product Deleted Successfully";
+            }
+            throw new Exception("Product not found");
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
