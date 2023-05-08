ffc7a17c029cc8395b6b53b79c14466e802b4612
Nikhil <nikhilshah.net@hotmail.com>
2023-05-08 18:09:36 +0530

    Fetch APi Call

---
+++ Day 8/apicalls_clientside/apicalls_clientside/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace apicalls_clientside.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
