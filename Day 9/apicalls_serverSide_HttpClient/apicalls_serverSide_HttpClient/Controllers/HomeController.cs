34cf4061b13fc921a4085832356d8bda1b4ff16e
Nikhil <nikhilshah.net@hotmail.com>
2023-05-09 18:30:23 +0530

    Day 9 - HttpClient Call

---
+++ Day 9/apicalls_serverSide_HttpClient/apicalls_serverSide_HttpClient/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using apicalls_serverSide_HttpClient.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace apicalls_serverSide_HttpClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        public IActionResult ShowComments()
+        {
+            CommentsModel modelObj = new CommentsModel();
+            ViewBag.data = modelObj.getComments(); // this will make a HttpClient call and get the data
+            return View();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Day 9/apicalls_serverSide_HttpClient/apicalls_serverSide_HttpClient/Models/CommentsModel.cs
@@ -0,0 +1,53 @@
+﻿namespace apicalls_serverSide_HttpClient.Models
+{
+    public class CommentsModel
+    {
+        public int postId { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string body { get; set; }
+
+        List<CommentsModel> commentsData = new List<CommentsModel>();
+
+        public List<CommentsModel> getComments()
+        {
+            //we will make the call here useing HttpClient 
+
+            HttpClient client = new HttpClient();
+            string url = "https://jsonplaceholder.typicode.com/comments";
+
+            //we need to clear the default datatype format from the calling environment,
+            //google chrome has a default format of XML
+            //IE has JSON
+            //Windows APP has Binary text as default
+            //Linux system has their own format, and so as Mac
+
+            client.DefaultRequestHeaders.Accept.Clear();
+            //we need to add JSON as a format
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            var call = client.GetAsync(url).Result;
+            //if call successful, we will read the data, else throw excecption
+            if (call.IsSuccessStatusCode)
+            {
+                var result = call.Content.ReadAsAsync<List<CommentsModel>>();
+                result.Wait();
+                commentsData = result.Result;
+                return commentsData;
+            }
+            else
+            {
+                throw new Exception("Could not get data from server");
+            }
+
+
+            return commentsData;
+        }
+
+    }
+}
+
+
+
+
